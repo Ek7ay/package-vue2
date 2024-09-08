87818598a0191b93ef63f3211836c11e7f9e14a6
曹彦平 <qxjj7ay@163.com>
2024-09-08 20:05:12 +0800

    组件版的loading

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <ul class="box">
+      <li v-for="(item,index) in data" :key="index">{{ item.name }}</li>
+    </ul> 
+    <div class="loading-wrapper">
+      <Loading v-show="data.length === 0"></Loading>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Loading from './directives/loading'
 
 export default {
   name: 'App',
   components: {
-
+    Loading
+  },
+  data() {
+    return {
+      data: []
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.data = [{
+        name: 'Welcome to Your Vue.js App'
+      }, {
+        name: 'Welcome to Vue.js App'
+      }, {
+        name: 'Welcome to Vue.js App'
+      }, {
+        name: 'Welcome Your Vue.js App'
+      }, {
+        name: 'Welcome to Your Vue.js App'
+      }]
+    }, 3000)
   }
 }
 </script>
@@ -20,8 +45,20 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  .box {
+    width: 600px;
+    height: 500px;
+    li {
+      list-style: none;
+    }
+  }
+
+  .loading-wrapper {
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 }
 </style>
---
+++ src/directives/loading/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="loading">
+        <img src="../../assets/apha-loding——2.gif" width="70" height="70">
+        <p class="title">{{ title }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: '正在加载中...',
+        },
+    },
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+        console.log('This is page loading');
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+.loading {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .title {
+        font-size: 20px;
+        line-height: 20px;
+    }
+}
+</style>
