84c12466193d648a83af14f61b4fcf1632514ba0
曹彦平 <qxjj7ay@163.com>
2024-09-09 15:26:07 +0800

    生成一张二维码图片

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <div class="list-wrapper" v-loading="loading">
+
+    <img width="200" :src="'data:image/png;base64,' + src" alt="">
+
+    <!-- <div class="list-wrapper" v-loading="loading">
       <ul>
         <li v-for="(item, index) in data" :key="index">{{ item.name }}</li>
       </ul>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -19,7 +22,8 @@ export default {
   data() {
     return {
       data: [],
-      title: 'Loading...'
+      title: 'Loading...',
+      src: 'iVBORw0KGgoAAAANSUhEUgAAALcAAAC3CAYAAABQbs+fAAAElElEQVR42u3dUY4iMQwFQO5/6d0zoEn8nkNZmi9gFNKVlow7zuefEI/GxxQIuIWAWwi4hYBbCLiFgFvALcTruD+fz+jf8S85PP6/ft9vX//r/J0eb4sPuOGGG2644YYb7s24b+Nrw316vN9+Pj3+2/NxywfccMMNN9xwww33S7inE6jbWLZ9vv39qZsJ3HDDDTfccMMNN9xzuG8nPNOL8a+L5XYRCG644YYbbrjhhhvu93FPJ1BtD15N44cbbrjhhhtuuOGG+z7u6QRsejFsL9psT9jhhhtuuOGGG264fxF3+4ZZr2dft/sdbrjhhgduuL0O9/e40zH9oM/t8bYnsK8E3HDDDTfccMMN9ybc0wnFNKbXixrpxpypRqhwww033HDDDTfcm3GnH7a/Pfm3E962m8HpxdNys4AbbrjhhhtuuOHehHu6SDOdYN0uikw3s29vHDpVZIMbbrjhhhtuuOHehDt9QFI6QZnGN734002J4IYbbrjhhhtuuOHOHwraVkRoK0Ldnv90UyS44YYbbrjhhhtuuPuKLOkiSjpBbTs09unm83DDDTfccMMNN9yHcE9P7ulIN6Nva8qTPkBqKuCGG2644YYbbrg3405jPb3Yto237WbRtjkCbrjhhhtuuOGGeyPu6S9zu0iQfpAp/f/amuhUN8KEG2644YYbbrjhLvmdezqBa3sQ6vbn2zf4thWx4IYbbrjhhhtuuF/A3d48flujy+mmRen3TxXN4IYbbrjhhhtuuDfhbkuQ2hPS4xcp3HQoffOAG2644YYbbrjh/kXc04eUpg91bX0Y/9RiaNuscepmAzfccMMNN9xww70Zd1sRoH1zwm287YeqrtogDDfccMMNN9xwwx3CPZ0gbpm8qcV9OsFv2zBcvUEYbrjhhhtuuOGGewh328P26c0FbeN9LQFeXcSBG2644YYbbrjhPpRQth1I1P4gVdvi34YfbrjhhhtuuOGGG+785NwumqSLGtOLtf1mBTfccMMNN9xww/0i7rYmOa8VWW7jeS2hhhtuuOGGG2644YY73ziyffzbNm9sbRoEN9xwww033HDDvQl3OuF5vTFmukjUlsCfWkxwww033HDDDTfcm3CnH45PJ1TpizndCHR6PNHNCnDDDTfccMMNN9xLcLcVNW5jvo1jW7RghhtuuOGGG2644X4Z9/TFam/UeLuIdHu+XjkwC2644YYbbrjhhvtl3Oki0G0c6fG1bfidPuQWbrjhhhtuuOGG+0Xc24oE6cWV/nxbkWlVl1e44YYbbrjhhhvukt+5t23YTRd5bieQbYsxfb3ghhtuuOGGG264X8B9G8/05LVvqJ0qeqSKYLfGDzfccMMNN9xww/0S7vaLldqgOnWzaC9ypZr/ww033HDDDTfccMN9D/f0gUetF7c1oV1VxIEbbrjhhhtuuOGGO1I02F50eS1BhxtuuOGGG2644f4F3NNFjdbJTI03fWDV7esLN9xwww033HDD/Yu4pzcIpy9+OgHe1hy/JeGEG2644YYbbrjh3oRbiI0Bt4BbCLiFgFsIuIWAWwi4BdxCwC1EYfwHWtibYQ08HCcAAAAASUVORK5CYII=',
     }
   },
   computed: {
